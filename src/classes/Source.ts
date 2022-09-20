ccc6fff587e6fbe75b45911f8db7d1c78c7b9c8b
Mantton <mantton.official@gmail.com>
2022-09-19 20:31:32 -0400

    fix: file names

---
+++ src/classes/Source.ts
@@ -0,0 +1,86 @@
+import {
+  SourceInfo,
+  RunnerType,
+  Content,
+  Chapter,
+  ChapterData,
+  SearchRequest,
+  SearchSort,
+  PagedResult,
+  Filter,
+  Property,
+  URLContentIdentifier,
+  Tag,
+  TrackerInfo,
+  Highlight,
+  User,
+  PreferenceGroup,
+  SyncedContent,
+  CollectionExcerpt,
+  ReadingFlag,
+  ExploreCollection,
+  NetworkRequest,
+} from "../types";
+import { DaisukeRunner } from "./Runner";
+
+export abstract class Source implements DaisukeRunner {
+  abstract readonly info: SourceInfo;
+  readonly type: RunnerType = RunnerType.CONTENT_SOURCE;
+
+  // Content
+  abstract getContent(contentId: string): Promise<Content>;
+  abstract getChapters(contentId: string): Promise<Chapter[]>;
+  abstract getChapterData(
+    contentId: string,
+    chapterId: string
+  ): Promise<ChapterData>;
+
+  // * Searching
+  abstract getSearchResults(query: SearchRequest): Promise<PagedResult>;
+  getSearchFilters?(): Promise<Filter[]>;
+  getSearchSorters?(): Promise<SearchSort[]>;
+
+  // * Explore
+  createExploreCollections?(): Promise<CollectionExcerpt[]>;
+  resolveExploreCollection?(
+    excerpt: CollectionExcerpt
+  ): Promise<ExploreCollection>;
+  getExplorePageTags?(): Promise<Tag[]>;
+
+  // Tags
+  abstract getSourceTags(): Promise<Property[]>;
+
+  // Source Handlers
+  handleIdentifierForUrl?(url: string): Promise<URLContentIdentifier | null>;
+  handleMatchTracker?(trackerInfo: TrackerInfo): Promise<Highlight[]>;
+
+  //  Source Preferences
+  getUserPreferences?(): Promise<PreferenceGroup[]>;
+
+  // Source State Events
+  onSourceLoaded?(): Promise<void>;
+  onContentsAddedToLibrary?(ids: string[]): Promise<void>;
+  onContentsRemovedFromLibrary?(ids: string[]): Promise<void>;
+  onChaptersCompleted?(contentId: string, chapterIds: string[]): Promise<void>;
+  onContentsReadingFlagChanged?(
+    ids: string[],
+    flag: ReadingFlag
+  ): Promise<void>;
+
+  // Request Events
+  willAttemptCloudflareVerification?(): Promise<string>;
+  willRequestImage?(request: NetworkRequest): Promise<NetworkRequest>;
+  // Authentication
+  handleBasicAuth?(identifier: string, password: string): Promise<void>;
+  getAuthenticatedUser?(): Promise<User | null>;
+  handleUserSignOut?(): Promise<void>;
+
+  // TODO: WebView & OAuth Auth
+  // handleWebViewAuth?(): Promise<void>;
+  // getOAuthRequest?(): Promise<NetworkRequest>;
+  // handleOAuthResponse?(): Promise<NetworkResponse>;
+
+  // Sync Related
+  getUserLibrary?(): Promise<SyncedContent[]>;
+  getReadChapterMarkers?(contentId: string): Promise<string[]>;
+}
---
+++ src/types/core/Runner.ts
@@ -0,0 +1,26 @@
+import { AuthMethod } from "./Authentication";
+
+export type RunnerInfo = {
+  readonly id: string;
+  readonly name: string;
+  readonly version: number;
+  readonly authors?: string[];
+  readonly minSupportedAppVersion?: string;
+};
+
+export enum RunnerType {
+  CONTENT_SOURCE,
+  SERVICE_INTERACTOR,
+}
+
+export type SourceInfo = RunnerInfo & {
+  readonly website: string;
+  readonly supportedLanguages: string[];
+  readonly hasExplorePage: boolean;
+  readonly primarilyAdultContent: boolean;
+  readonly thumbnail?: string;
+
+  readonly VXI?: string;
+  readonly authMethod?: AuthMethod;
+  readonly contentSync?: boolean;
+};
